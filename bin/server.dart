cc8bdc0381e6fb5153fbe4004a8cb553a553a786
Ivan Semkin <ivan@semkin.ru>
2025-09-19 23:46:41 +0300

    Use `fl-common-lint` analyzer ruleset

--- bin/server.dart
+++ bin/server.dart
@@ -5,25 +5,25 @@ import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 // Configure routes.
-final _router = Router()
-  ..get('/<message>', _echoHandler);
+final Router _router = Router()..get('/<message>', _echoHandler);
 
-Response _echoHandler(Request request) {
-  final message = request.params['message'];
+Response _echoHandler(final Request request) {
+  final String? message = request.params['message'];
   return Response.ok('$message\n');
 }
 
-void main(List<String> args) async {
+void main(final List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
+  final InternetAddress ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final handler = Pipeline()
+  final Handler handler = const Pipeline()
       .addMiddleware(logRequests())
       .addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
+  final int port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final HttpServer server = await serve(handler, ip, port);
+  // ignore: avoid_print, Initialization message useful for verifying Docker configuration
   print('Server listening on port ${server.port}');
 }
