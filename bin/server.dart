81cc69a274a5145799721230e02cc4b37fc026d8
Ivan Semkin <ivan@semkin.ru>
2025-09-20 00:25:08 +0300

    Generate plist files on the fly

--- bin/server.dart
+++ bin/server.dart
@@ -1,18 +1,62 @@
 import 'dart:io';
 
+import 'package:propertylistserialization/propertylistserialization.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 // Configure routes.
-final Router _router = Router()..get('/<message>', _echoHandler);
+final Router _router = Router()
+  ..get('/<bundleId>/<version>/<title>', _echoHandler);
 
 Response _echoHandler(final Request request) {
-  final String? message = request.params['message'];
-  return Response.ok(
-    '$message\n',
-    headers: <String, Object>{'Content-Type': 'application/octet-stream'},
-  );
+  final String? bundleId = request.params['bundleId'];
+  final String? version = request.params['version'];
+  final String? title = request.params['title'];
+
+  if (bundleId != null && version != null && title != null) {
+    final String fileName = '$title-$version';
+    final String downloadUrl = 'https://download.vanyasem.ru/ipa/$fileName.ipa';
+    // https://developer.apple.com/documentation/devicemanagement/manifesturl/itemsitem
+    final Map<String, Object> dict = <String, Object>{
+      'items': <Object>[
+        <String, Object>{
+          'assets': <Object>[
+            <String, Object>{'kind': 'software-package', 'url': downloadUrl},
+          ],
+          'metadata': <String, Object>{
+            'bundle-identifier': bundleId,
+            'kind': 'software',
+            'title': title,
+          },
+        },
+      ],
+    };
+
+    try {
+      final String result = PropertyListSerialization.stringWithPropertyList(
+        dict,
+      );
+      return Response.ok(
+        result,
+        headers: <String, Object>{'Content-Type': 'application/octet-stream'},
+      );
+    } on PropertyListWriteStreamException catch (_) {
+      return Response.internalServerError();
+    }
+  } else {
+    String error = '';
+    if (bundleId == null) {
+      error += 'Missing bundleId\n';
+    }
+    if (version == null) {
+      error += 'Missing version\n';
+    }
+    if (title == null) {
+      error += 'Missing title\n';
+    }
+    return Response.badRequest(body: error);
+  }
 }
 
 void main(final List<String> args) async {
